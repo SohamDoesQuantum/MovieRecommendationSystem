0a949fb634fb3958595ebf0fa813e2c9e483da6e
Soham Debnath <sohamdebnath.edu@gmail.com>
2026-06-25 11:33:18 +0530

    Refactor movie recommendation logic and UI

--- app.py
+++ app.py
@@ -1,63 +1,59 @@
-import streamlit as st
-import pandas as pd
 import pickle
-import os
-import urllib.request
+import streamlit as st
+import requests
 
-st.title("Movie Recommendation System")
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
 
-@st.cache_resource
-def load_data():
-    movies_url = "https://onedrive.live.com/download?cid=BDC93B19D04E2812&resid=BDC93B19D04E2812%21174246&authkey=AX6Rj9OSg3pTr1srxkgcHgI"
-    similarity_url = "https://onedrive.live.com/download?cid=BDC93B19D04E2812&resid=BDC93B19D04E2812%21174317&authkey=AQD5fqpys6RaibbBXN1trHU"
-    
-    # Force delete broken/empty files from previous failed attempts
-    for file in ['movies_dict.pkl', 'similarity.pkl']:
-        if os.path.exists(file) and os.path.getsize(file) < 100000:  # If file is too small, it's just broken HTML text
-            os.remove(file)
-            
-    # Download with a User-Agent header so OneDrive allows the direct download
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)')]
-    urllib.request.install_opener(opener)
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
 
-    if not os.path.exists('movies_dict.pkl'):
-        with st.spinner("Downloading movies data..."):
-            urllib.request.urlretrieve(movies_url, 'movies_dict.pkl')
-        
-    if not os.path.exists('similarity.pkl'):
-        with st.spinner("Downloading similarity matrix (this may take a minute)..."):
-            urllib.request.urlretrieve(similarity_url, 'similarity.pkl')
+    return recommended_movie_names,recommended_movie_posters
 
-    movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
-    similarity = pickle.load(open('similarity.pkl', 'rb'))
-    
-    return pd.DataFrame(movies_dict), similarity
 
-# Load data smoothly
-movies, similarity = load_data()
+st.header('Movie Recommender System')
+movies = pickle.load(open('model/movie_list.pkl','rb'))
+similarity = pickle.load(open('model/similarity.pkl','rb'))
 
-# Dropdown menu for movie selection
-selected_movie_name = st.selectbox(
-    'Type or select a movie to get recommendations:',
-    movies['title'].values
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
 )
 
-# Recommendation function from your notebook
-def recommend(movie):
-    movie_index = movies[movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    
-    # Grabs top 5 matches
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-    
-    recommended_movies = []
-    for i in movies_list:
-        recommended_movies.append(movies.iloc[i[0]].title)
-    return recommended_movies
+if st.button('Show Recommendation'):
+    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.beta_columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
 
-# Button trigger
-if st.button('Recommend'):
-    recommendations = recommend(selected_movie_name)
-    for movie in recommendations:
-        st.write(movie)
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
+
+
+
+
